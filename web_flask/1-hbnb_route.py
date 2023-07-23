0c39284604cc0a8c185f1be8d86e3074e85db305
oluphunmmy <olufunmilayoagboola@gmail.com>
2023-07-23 07:28:15 -0700

    web frame

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ a script that starts a Flask web application
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
 """
 from flask import Flask
 app = Flash(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """ A function that display Hello HBNB! """
     return "Hello HBNB!"
 
